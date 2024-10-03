8ff75746c5dce56cfb18f404b420224322b32d5d
IT24100527 <it24100527@my.sliit.lk>
2024-10-03 20:44:25 +0530

    Add files via upload

---
+++ Q2/IT24100527Lab7Q2B.java
@@ -0,0 +1,15 @@
+public class IT24100527Lab7Q2B {
+    public static void main(String[] args) {
+
+        for (int number = 1; number <= 5; number++) {
+
+            System.out.print(number + " - ");
+            
+            for (int star = 1; star <= number; star++) {
+                System.out.print("* ");
+            }
+            
+            System.out.println();
+        }
+    }
+}
